c0c4980257280d6b551a86751101dcbbafda081b
qhdltmwhs <qhdltmwhs@gmail.com>
2021-03-02 10:25:18 +0900

    l

--- edu/src/com/edu/test/Calc2.java
+++ edu/src/com/edu/test/Calc2.java
@@ -6,9 +6,11 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class AddServlet
@@ -18,6 +20,8 @@ public class Calc2 extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletContext application = request.getServletContext();
+		HttpSession session = request.getSession();
+		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
@@ -33,10 +37,11 @@ public class Calc2 extends HttpServlet {
 		}
 		if(op.equals("=")) {
 			
-			int x = (Integer)application.getAttribute("value");
+			//int x = (Integer)application.getAttribute("value");
+			int x = (Integer)session.getAttribute("value");
 			int y = v;
-			String operator = (String)application.getAttribute("op");
-			
+			//String operator = (String)application.getAttribute("op");
+			String operator = (String)session.getAttribute("op");
 			int result = 0;
 			
 			if(operator.equals("+")) {
@@ -47,8 +52,21 @@ public class Calc2 extends HttpServlet {
 			}
 			out.printf("result is %d\n", result);
 		}else {
-			application.setAttribute("value", v);
-			application.setAttribute("op", op);
+			//application.setAttribute("value", v);
+			//application.setAttribute("op", op);
+			
+			//session.setAttribute("value", v);
+			//session.setAttribute("op", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			valueCookie.setPath("/calc2");
+			valueCookie.setMaxAge(24*60*60);
+			opCookie.setPath("/calc2");
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
+			
+			response.sendRedirect("calc2.html");
 		}
 		
 		

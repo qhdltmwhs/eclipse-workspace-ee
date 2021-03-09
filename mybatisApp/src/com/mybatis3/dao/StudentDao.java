38709ed27723666e76d6ea31d641cd012d781792
qhdltmwhs <qhdltmwhs@gmail.com>
2021-03-09 15:46:32 +0900

    l

--- mybatisApp/src/com/mybatis3/dao/StudentDao.java
+++ mybatisApp/src/com/mybatis3/dao/StudentDao.java
@@ -26,7 +26,53 @@ public class StudentDao {
 			e.printStackTrace();
 		}
 	}
+	/*@@@@@@@@@@@@@@@@@@@@@@@INSERT@@@@@@@@@@@@@@@@@*/
+	public int insertStudent(Student student) {
+		SqlSession sqlSession=sqlSessionFactory.openSession(false);
+		int row=sqlSession.insert(NAMESPACE+"insertStudent", student);
+		sqlSession.commit();
+		sqlSession.close();
+		return row;
+	}
+	public int insertStudentBySequence1(Student student) {
+		SqlSession sqlSession=sqlSessionFactory.openSession(false);
+		int row=sqlSession.insert(NAMESPACE+"insertStudentBySequence1", student);
+		sqlSession.commit();
+		sqlSession.close();
+		return row;
+	}
+	public int insertStudentBySequence2(Student student) {
+		SqlSession sqlSession=sqlSessionFactory.openSession(false);
+		int row=sqlSession.insert(NAMESPACE+"insertStudentBySequence2", student);
+		sqlSession.commit();
+		sqlSession.close();
+		return row;
+	}
+	
 	/*@@@@@@@@@@@@@@@@@@@@@@@SELECT@@@@@@@@@@@@@@@@@*/
+	
+	/*
+	 resultMap 
+	 */
+	public Student findStudentByIdResultMap(Integer studId) {
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		Student findStudent=
+				sqlSession.selectOne(NAMESPACE+"findStudentByIdResultMap",studId);
+		sqlSession.close();
+		return findStudent;
+	}
+	public List<Student> findAllStudentsResultMap() {
+		SqlSession sqlSession=sqlSessionFactory.openSession(false);
+		List<Student> studentList=
+				sqlSession.selectList(NAMESPACE+"findAllStudentsResultMap");
+		sqlSession.commit();
+		sqlSession.close();
+		return studentList;
+	}
+	
+	/*
+	 resultType Dto 
+	*/
 	public Student findStudentById(Integer studId) {
 		SqlSession sqlSession=sqlSessionFactory.openSession(true);
 		Student findStudent=
@@ -42,6 +88,9 @@ public class StudentDao {
 		sqlSession.close();
 		return studentList;
 	}
+	/*
+	 resultType Wrapper(String) 
+	 */
 	public String findStudentNameById(Integer userId) {
 		SqlSession sqlSession=sqlSessionFactory.openSession(false);
 		String studentName=
@@ -58,7 +107,21 @@ public class StudentDao {
 		sqlSession.close();
 		return studentNameList;
 	}
-	
+	/*
+	 resultType Map
+	 */
+	public Map findStudentByIdMap(Integer userId) {
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		Map studentMap=sqlSession.selectOne(NAMESPACE+"findStudentByIdMap",userId);
+		sqlSession.close();
+		return studentMap;
+	}
+	public List<Map> findAllStudentsMapList() {
+		SqlSession sqlSession=sqlSessionFactory.openSession(true);
+		List<Map> studentMapList=sqlSession.selectList(NAMESPACE+"findAllStudentsMapList");
+		sqlSession.close();
+		return studentMapList;
+	}
 	
 }
 
--- mybatisApp/src/com/mybatis3/dao/StudentDaoMain.java
+++ mybatisApp/src/com/mybatis3/dao/StudentDaoMain.java
@@ -1,5 +1,10 @@
 package com.mybatis3.dao;
 
+import java.util.Date;
+import java.util.Map;
+
+import com.mybatis3.domain.Student;
+
 public class StudentDaoMain {
 
 	public static void main(String[] args) {
@@ -13,25 +18,41 @@ public class StudentDaoMain {
 		System.out.println(studentDao.findStudentNameById(3));
 		System.out.println("---------findStudentNameList-------------------------");
 		System.out.println(studentDao.findStudentNameList());
+		System.out.println("---------findStudentByIdMap--------------------------");
+		Map studentMap = studentDao.findStudentByIdMap(1);
+		System.out.println(studentMap.get("STUD_ID")+" "+
+						   studentMap.get("NAME")+" "+
+				           studentMap.get("EMAIL")+" "
+						   +studentMap.get("DOB"));
 		
-		
+		System.out.println("---------findAllStudentsMapList----------------------");
+		System.out.println(studentDao.findAllStudentsMapList());
 		System.out.println("---------findStudentByIdResultMap--------------------");
+		System.out.println(studentDao.findStudentByIdResultMap(3));
+		System.out.println("---------findAllStudentsResultMap---------");
+		System.out.println(studentDao.findAllStudentsResultMap());
 		System.out.println("---------insertStudent(Dto)--------------------------");
+		//System.out.println(studentDao.insertStudent(new Student(1000, "김천천", "1000@1000.co.kr", new Date())));
 		System.out.println("---------insertStudentBySequence1--------------------");
+		Student student1=new Student(null, "김시퀀스1", "seq1@gmail.com", new Date());
+		System.out.println(studentDao.insertStudentBySequence1(student1));
+		System.out.println("insert PK:"+student1.getStudId());
 		System.out.println("---------insertStudentBySequence2--------------------");
+		Student student2=new Student(null, "김시퀀스2", "seq2@gmail.com", new Date());
+		System.out.println(studentDao.insertStudentBySequence2(student2));
+		System.out.println("insert PK:"+student2.getStudId());
+		
 		System.out.println("---------updateStudent-------------------------------");
 		System.out.println("---------deleteStudentById---------------------------");
 		System.out.println("---------findStudentByIdWithAddress------------------");
 		System.out.println("---------findStudentByIdWithCourses------------------");
 		
-		System.out.println("---------findStudentByIdMap--------------------------");
-		
-		System.out.println("---------findAllStudentsMapList----------------------");
+	
 		System.out.println("---------updateStudentParamMap(parameter Map)--------");
 		System.out.println("---------findStudentByIdRangeParamMap(parameter Map)-");
 		System.out.println("---------findStudentsThreeParamMap(parameter Map)----");
 		/*
-		System.out.println("---------findAllStudentsResultMap---------");
+		
 		System.out.println("---------findStudentByIdNestedfindAddressById-----------");
 		System.out.println("---------updateStudentDynamicSql---------");
 		System.out.println("---------findStudentCount----------------");
